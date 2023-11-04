8a307f3c70c1648442b051e7a6d9dde4fcfa1399
Tarek Mostafa <tarek.r.mostafa@gmail.com>
2023-11-04 13:24:20 +0200

    convert iformfile to stream

--- src/Core/Application/Common/Helpers/Utilities.cs
+++ src/Core/Application/Common/Helpers/Utilities.cs
@@ -1,6 +1,8 @@
-﻿namespace Application.Common.Helpers;
+﻿using Microsoft.AspNetCore.Http;
 
-public class Utilities
+namespace Application.Common.Helpers;
+
+public static class Utilities
 {
     public static string GenerateRandomString(int length)
     {
@@ -29,4 +31,29 @@ public class Utilities
         var finalString = new string(stringChars);
         return int.Parse(finalString);
     }
+    public static  async Task<Stream> ConvertToStream(this IFormFile formFile)
+    {
+        if (formFile == null || formFile.Length == 0)
+        {
+            return null;
+        }
+
+        var stream = new MemoryStream();
+
+        await using (var fileStream = formFile.OpenReadStream())
+        {
+            await fileStream.CopyToAsync(stream);
+        }
+
+        stream.Seek(0, SeekOrigin.Begin); // Reset the stream position to the beginning
+
+        return stream;
+    }
+   
+
+
+
+
+
+
 }
